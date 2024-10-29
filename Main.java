050238111428e4a6fb07ef4b9743532c15c1e25b
jrcarre <56210822+arrejohnray@users.noreply.github.com>
2024-10-29 06:42:31 +0000

    Diamond

--- Main.java
+++ Main.java
@@ -1,1 +1,34 @@
 
+
+
+public class Main {
+    public static void main(String[] args)throws Exception {
+        int height = 6;
+
+        //Outer loop would be the row
+        //The inner loop should be the column
+        for(int row = 1; row <= height; row++){
+            for(int space = 0; space <= height - row; space++){
+                System.out.print(" ");
+            }
+        
+            for(int col = 1; col <= row * 2 - 1; col++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }        
+
+
+        for(int row = height - 1; row >= 1; row--){
+            for(int space = height; space > row - 1; space--){
+                System.out.print(" ");
+            }
+            for(int col = 1; col <= row * 2 - 1; col++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
