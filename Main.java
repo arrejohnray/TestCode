bd1faa6283c2c047d84214ff0323c64f5cd542e5
jrcarre <56210822+arrejohnray@users.noreply.github.com>
2024-11-05 01:17:13 +0000

    Test code 1

--- Main.java
+++ Main.java
@@ -2,33 +2,21 @@
 
 
 public class Main {
-    public static void main(String[] args)throws Exception {
-        int height = 6;
+   static String name;
+    String section;
+    String year;
+    int age;
 
-        //Outer loop would be the row
-        //The inner loop should be the column
-        for(int row = 1; row <= height; row++){
-            for(int space = 0; space <= height - row; space++){
-                System.out.print(" ");
-            }
-        
-            for(int col = 1; col <= row * 2 - 1; col++){
-                System.out.print("*");
-            }
-
-            System.out.println();
-        }        
+    public static void main(String args[]) {
+        Main student = new Main();
 
 
-        for(int row = height - 1; row >= 1; row--){
-            for(int space = height; space > row - 1; space--){
-                System.out.print(" ");
-            }
-            for(int col = 1; col <= row * 2 - 1; col++){
-                System.out.print("*");
-            }
+        name = "John";
 
-            System.out.println();
-        }
+
+        System.out.println(name);
+
+
+ 
     }
 }
---
+++ TestCodes/SampleCode1.java
@@ -0,0 +1,38 @@
+package TestCodes;
+
+/**
+ * SampleCode1
+ */
+public class SampleCode1 {
+
+    void sampleCode(){
+        int height = 6;
+
+        //Outer loop would be the row
+        //The inner loop should be the column
+        for(int row = 1; row <= height; row++){
+            for(int space = 0; space <= height - row; space++){
+                System.out.print(" ");
+            }
+        
+            for(int col = 1; col <= row * 2 - 1; col++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }        
+
+
+        for(int row = height - 1; row >= 1; row--){
+            for(int space = height; space > row - 1; space--){
+                System.out.print(" ");
+            }
+            for(int col = 1; col <= row * 2 - 1; col++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+    
+}
